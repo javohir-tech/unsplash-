77126bed2f23cb8ccac15a966d756f5c4c3fd22d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2024-11-14 02:23:32 +0500

    projecy update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,14 @@
 //react icons 
 import { FcStackOfPhotos } from "react-icons/fc";
+import { IoIosHeart } from "react-icons/io";
+import { FaMoon } from "react-icons/fa"
+import { IoSunny } from "react-icons/io5";
 
 
 //components 
 import NavbarLinks from "./NavbarLinks";
+//react router dom 
+import { Link } from "react-router-dom";
 
 
 function Navbar() {
@@ -13,13 +18,27 @@ function Navbar() {
                 <div className="container">
                     <header className='navbar'>
                         <div className="navbar-start">
-                            <FcStackOfPhotos className="w-8 h-8" />
+                            <Link to="/"><FcStackOfPhotos className="w-8 h-8" /></Link>
                         </div>
-                        <div className="navbar-center">
+                        <div className="navbar-center hidden md:flex">
                             <NavbarLinks />
                         </div>
-                        <div className='navbar-end'>
-                            
+                        <div className='navbar-end flex items-center gap-3'>
+                            <div>
+                                <IoIosHeart className="w-6 h-6" />
+                            </div>
+
+                            <label className="swap swap-rotate">
+                                {/* this hidden checkbox controls the state */}
+                                <input type="checkbox" className="theme-controller" value="synthwave" />
+
+                                {/* sun icon */}
+                                <IoSunny className='swap-off h-6 w-6 fill-current' />
+
+                                {/* moon icon */}
+                                <FaMoon className='swap-on h-5 w-5 fill-current' />
+                            </label>
+
                         </div>
                     </header>
                 </div>
