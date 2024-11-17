0619c23525a0929a43d11baa7eb40ab5c54643b8
Suvonov Javohir <suvonovjavohir625@gmail.com>
2024-11-18 02:00:53 +0500

    search update

--- src/Components/FormInput.jsx
+++ src/Components/FormInput.jsx
@@ -3,7 +3,6 @@ import { FaSearch } from "react-icons/fa";
 
 function FormInput({ type, placeholder, name }) {
     return (
-
         <label className="input input-bordered flex items-center gap-2 w-full input-sm  md:input-md">
             <input
                 type={type}
@@ -13,7 +12,6 @@ function FormInput({ type, placeholder, name }) {
             />
             <FaSearch className="h-4 w-4 opacity-70" />
         </label>
-
     )
 }
 
--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -1,10 +1,14 @@
 //components
+import { UseGlobalContext } from '../Hooks/useGlobalContext'
 import { FormInput } from './'
 
 //react rouder dom
 import { Form } from 'react-router-dom'
 
 function search() {
+
+    const {images, dispatch} = UseGlobalContext()
+
     return (
         <Form method="POST" className='max-w-96 mx-auto flex gap-2'>
             <FormInput type={"name"} placeholder={"Search"} name={"Search"} />
