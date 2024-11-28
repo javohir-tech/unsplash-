d02cffd57d179de3f98f2eb6fb93607225b19245
Suvonov Javohir <suvonovjavohir625@gmail.com>
2024-11-29 00:12:00 +0500

    use firebase

---
+++ src/firebase/useFireBaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCge1OXV7pW2XIev6wsigR_S94JtwJ3Ed0",
+  authDomain: "my-splash-e941b.firebaseapp.com",
+  projectId: "my-splash-e941b",
+  storageBucket: "my-splash-e941b.firebasestorage.app",
+  messagingSenderId: "818614937986",
+  appId: "1:818614937986:web:cce4c1011292af72c1079b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
